3b3cd24ad9afc5188c1f2d845fac80384b609b19
Дмитрий К <dim-2g@yandex.ru>
2020-09-04 16:51:15 +0300

    вывод множдественного редирект, кнопки

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import {Nav} from './components/Nav';
 import PreloaderCell from "./components/PreloaderCell";
+import Axios from "axios";
+import classNames from 'classnames' 
+import Parser from "html-react-parser";
 import './App.css';
 import 'materialize-css';
-import Axios from "axios";
+
 
 
 const initialState = [];
 
 function App() {
+  const [text, setText] = useState('')
   const [pages, setPages] = useState(initialState)
   const [finish, setFinish] = useState(false)
+  const [start, setStart] = useState(false)
 
   const onChangeHandler = (e) => {
+    setText(e.target.value)
     const urls = e.target.value.trim().split("\n");
     const userPages = urls.map((item, index) => {
       return {
@@ -27,15 +33,7 @@ function App() {
     setPages(userPages);
   }
 
-  const findNextPage = () => {
-    const nextPage = pages.find(item => {
-      if (item.verified == false) {
-        return item
-      }
-      return null
-    })
-    return nextPage
-  }
+  const findNextPage = () => pages.find(item => item.verified === false ? item : null)
 
   const checkOne = async (nextPage) => {
     //Ставим метку загрузки
@@ -48,9 +46,9 @@ function App() {
     setPages(newPages)
 
     //отправляем запрос
-    Axios.post(`/responser/?timestamp=${new Date().getTime()}`, {
-      url: nextPage.url
-    })
+    await Axios.post(`/responser/?timestamp=${new Date().getTime()}`, {
+          url: nextPage.url
+        })
         .then(res => {
           const newPagesLoad = pages.map(item => {
             if (item.url === nextPage.url) {
@@ -58,6 +56,7 @@ function App() {
               item.verified = true
               item.code = res.data.code
               item.multyRedirect = !!res.data.info.redirect_count
+              item.chains = res.data.chains || []
             }
             return item
           })
@@ -68,17 +67,26 @@ function App() {
         })
   }
 
+  const run = () => {
+    setStart(true)
+    check()
+  }
+
   const check = async () => {
-    //while (true) {
-    console.log('start');
       const nextPage = findNextPage();
       if (!nextPage) {
         setFinish(true)
-        return
+        return null
       }
       await checkOne(nextPage);
-      console.log('stop');
-    //}
+      check();
+  }
+
+  const reinit = () => {
+    setText('')
+    setPages(initialState)
+    setStart(false)
+    setFinish(false)
   }
 
   useEffect(() => {
@@ -86,6 +94,12 @@ function App() {
   }, [pages])
 
       //console.log('state', pages);
+  const formatChains = (chains) => {
+      const result = chains.map(chain => {
+          return `${chain.http_code} : ${chain.location}`
+      })
+      return result.join("<br />")
+  }
 
   return (
       <>
@@ -98,7 +112,7 @@ function App() {
             Проверяем отклики страниц. Вы можете загрузить список страниц для массовой проверки<br />
             Каждый адрес должен начинаться с новой строки
           </blockquote>
-
+          
           <div className="row">
             <form className="col s12">
               <div className="row">
@@ -108,6 +122,8 @@ function App() {
                       id="icon_prefix2"
                       className="materialize-textarea"
                       onChange={(e) => onChangeHandler(e)}
+                      disabled={start}
+                      value={text}
                   ></textarea>
                   <label htmlFor="icon_prefix2">Список страниц</label>
                 </div>
@@ -118,13 +134,23 @@ function App() {
           {pages && pages.length > 0 && (
               <div>
                 <div className="responser-controls">
-                  <button
-                      className="waves-effect waves-light btn"
-                      onClick={() => check()}
-                  >
-                    <i className="material-icons left">play_arrow</i>
-                    Начать проверку
-                  </button>
+                    <button
+                        className="waves-effect waves-light btn"
+                        onClick={() => run()}
+                        disabled={start}
+                    >
+                      <i className="material-icons left">play_arrow</i>
+                      Начать проверку
+                    </button>
+                    {finish && 
+                      <button
+                          className="waves-effect waves-light btn red lighten-2"
+                          onClick={() => reinit()}
+                      >
+                        <i className="material-icons left">autorenew</i>
+                        Новая проверка
+                      </button>
+                    }
                 </div>
                 <table>
                   <thead>
@@ -132,27 +158,35 @@ function App() {
                     <th>##</th>
                     <th>Адрес страницы</th>
                     <th>Код ответа</th>
-                    <th>Множественный редирект</th>
+                    <th width="400">Множественный редирект</th>
                   </tr>
                   </thead>
                   <tbody>
                   {pages.map((item, index) => {
+                    const badgeClass = classNames(
+                      'new',
+                      'badge',
+                      {'orange': item.code === 301},
+                      {'red': item.code === 404}
+                    )
                     return (
                         <tr key={item.url}>
                           <td>{index+1}</td>
                           <td>{item.url}</td>
-                          <td>
+                          <td className="centered">
                             {item.checking && !item.verified &&
                                 <PreloaderCell />
                             }
                             {!item.checking && item.verified &&
-                                <span className="new badge" data-badge-caption="">{item.code}</span>
+                                <span className={badgeClass} data-badge-caption="">{item.code}</span>
                             }
                             </td>
                           <td>
-                            <pre>
-                            {}
-                            </pre>
+                            <small>
+                                {item.chains && 
+                                  Parser(formatChains(item.chains))
+                                }
+                            </small>
                           </td>
                         </tr>
                     );
@@ -162,6 +196,12 @@ function App() {
               </div>
           )}
 
+
+          <div>
+              https://www.yournail.ru/<br />
+              http://yournail.ru/manikyur/na-novyij-god/<br />
+              https://www.yournail.ru/manikyur/k-23-fevralya/<br />
+          </div>
 
 
         </div>
